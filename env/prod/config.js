be1f475fb54e7cc25f8f83c8618caa7fa00641b0
Matt <hello@mattnull.com>
2013-07-15 14:35:43 -0600

    up

--- env/prod/config.js
+++ env/prod/config.js
@@ -1,13 +1,5 @@
 /** Config for individual dev environment **/
 
-namespace = module.exports ? module.exports : window.CONFIG
-namespace = {
-	baseURI : '', //base URI for server ex. http://localhost
-	// PhoneGap Build
-	PGB : { 
-		username : '',
-		password : '',
-		// can be found at http://build.phonegap.com
-		appID : ''
-	}
+window.CONFIG = {
+	baseURI : '' //base URI for server ex. http://localhost
 }
